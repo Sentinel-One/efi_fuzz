cfe6a1187bb3c40e55dd459a9e376f94ada80f36
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2020-11-25 14:44:11 +0200

    Override Qiling's SMM_SW_DISPATCH2_PROTOCOL with our own version.

--- protocols/smm/__init__.py
+++ protocols/smm/__init__.py
@@ -1,5 +1,6 @@
 from .smm_cpu_protocol import install_EFI_SMM_CPU_PROTOCOL
 from .smm_sw_dispatch_protocol import install_EFI_SMM_SW_DISPATCH_PROTOCOL
+from .smm_sw_dispatch2_protocol import install_EFI_SMM_SW_DISPATCH2_PROTOCOL
 from .smm_sx_dispatch_protocol import install_EFI_SMM_SX_DISPATCH_PROTOCOL
 from .smm_base_protocol import install_EFI_SMM_BASE_PROTOCOL
 from .smm_variable_protocol import install_EFI_SMM_VARIABLE_PROTOCOL
@@ -31,6 +32,11 @@ def init(ql):
     (ptr, smm_sw_dispatch_protocol) = install_EFI_SMM_SW_DISPATCH_PROTOCOL(ql, ptr)
     ql.loader.handle_dict[1][EFI_SMM_SW_DISPATCH_PROTOCOL_GUID] = smm_sw_dispatch_protocol_ptr
 
+    # EFI_SMM_SW_DISPATCH2_PROTOCOL
+    smm_sw_dispatch2_protocol_ptr = ptr
+    (ptr, smm_sw_dispatch2_protocol) = install_EFI_SMM_SW_DISPATCH2_PROTOCOL(ql, ptr)
+    ql.loader.handle_dict[1][EFI_SMM_SW_DISPATCH2_PROTOCOL_GUID] = smm_sw_dispatch2_protocol_ptr
+
     # EFI_SMM_SX_DISPATCH_PROTOCOL
     smm_sx_dispatch_protocol_ptr = ptr
     (ptr, smm_sx_dispatch_protocol) = install_EFI_SMM_SX_DISPATCH_PROTOCOL(ql, ptr)
@@ -49,6 +55,7 @@ def init(ql):
     # Serialize all protocols to memory
     ql.mem.write(smm_cpu_protocol_ptr, convert_struct_to_bytes(efi_smm_cpu_protocol))
     ql.mem.write(smm_sw_dispatch_protocol_ptr, convert_struct_to_bytes(smm_sw_dispatch_protocol))
+    ql.mem.write(smm_sw_dispatch2_protocol_ptr, convert_struct_to_bytes(smm_sw_dispatch2_protocol))
     ql.mem.write(smm_sx_dispatch_protocol_ptr, convert_struct_to_bytes(smm_sx_dispatch_protocol))
     ql.mem.write(smm_base_protocol_ptr, convert_struct_to_bytes(smm_base_protocol))
     ql.mem.write(smm_variable_protocol_ptr, convert_struct_to_bytes(smm_variable_protocol))
