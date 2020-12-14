da201181483aee9981605d6b792fcda00d4db3cf
liba2k <liba2k@gmail.com>
2020-12-13 20:12:42 -0800

    Adding support for triggering smi_handlers registered using EFI_MM_SYSTEM_TABLE->hook_mm_interrupt_register. We bypass the communicate mechanism and call them directly.

--- smm/protocols/__init__.py
+++ smm/protocols/__init__.py
@@ -6,7 +6,10 @@ from .smm_base_protocol import install_EFI_SMM_BASE_PROTOCOL
 from .smm_variable_protocol import install_EFI_SMM_VARIABLE_PROTOCOL
 from .smm_access_protocol import install_EFI_SMM_ACCESS_PROTOCOL
 from .guids import *
-from qiling.os.uefi.utils import convert_struct_to_bytes
+from qiling.os.uefi.utils import convert_struct_to_bytes, write_int64
+from qiling.os.uefi.const import *
+from ..swsmi import EFI_SMM_SW_CONTEXT, trigger_swsmi
+import ctypes
 
 def install(ql):
     # Allocate and initialize the protocols buffer
@@ -57,3 +60,30 @@ def install(ql):
     ql.mem.write(smm_base_protocol_ptr, convert_struct_to_bytes(smm_base_protocol))
     ql.mem.write(smm_variable_protocol_ptr, convert_struct_to_bytes(smm_variable_protocol))
     ql.mem.write(smm_access_protocol_ptr, convert_struct_to_bytes(smm_access_protocol))
+
+    ql.os.smm = SmmState(ql)
+
+    def hook_InSmm(ql, address, params):
+        nonlocal in_smm
+        write_int64(ql, params["InSmram"], in_smm)
+
+    # Replace 'InSmm' to correctly report whether or not we're executing an SMM module.
+    ql.set_api("InSmm", hook_InSmm)
+
+    def after_module_execution_callback(ql, number_of_modules_left):
+        if number_of_modules_left == 0:
+            return trigger_swsmi(ql)
+        return False
+
+    ql.os.after_module_execution_callbacks.append(after_module_execution_callback)
+    
+    init_GetCapabilities(ql)
+
+    def hook_mm_interrupt_register(ql, address, params):
+        smi_num = 0
+        params['RegisterContext'] = 0
+        params['DispatchFunction'] = params["Handler"]
+        ql.os.smm.swsmi_handlers.append((smi_num, params))
+        return EFI_SUCCESS
+
+    ql.set_api("mm_interrupt_register", hook_mm_interrupt_register)
