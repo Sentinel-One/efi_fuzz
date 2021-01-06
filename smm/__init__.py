e49292e02a28b10a04a6fbbb60b75d3a46ab8f20
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-01-06 14:25:36 +0200

    Refactor SMI invocation.

--- smm/__init__.py
+++ smm/__init__.py
@@ -1,32 +1,38 @@
 from . import protocols
 from qiling.os.uefi.utils import convert_struct_to_bytes, write_int64
 from qiling.os.memory import QlMemoryHeap
-from .swsmi import EFI_SMM_SW_CONTEXT, trigger_swsmi
-import ctypes
+from .swsmi import trigger_swsmi
 
 class SmmState(object):
+
+    PAGE_SIZE = 0x1000
+
     def __init__(self, ql):
-        self.swsmi_handlers = []
+        self.swsmi_handlers = {}
         self.smbase = int(ql.os.profile.get("SMM", "smbase"), 0)
         self.smram_size = int(ql.os.profile.get("SMM", "smram_size"), 0)
         self.heap_size = int(ql.os.profile.get("SMM", "heap_size"), 0)
         self.swsmi_args = {}
         
-        # Communication buffer
-        self.comm_buffer = ql.os.heap.alloc(ctypes.sizeof(EFI_SMM_SW_CONTEXT))
-        self.comm_buffer_size = ql.os.heap.alloc(ctypes.sizeof(ctypes.c_void_p))
-        ql.mem.write(self.comm_buffer_size, ctypes.sizeof(EFI_SMM_SW_CONTEXT).to_bytes(ctypes.sizeof(ctypes.c_void_p), 'little'))
-        
         if self.smram_size - self.heap_size < 0x10000:
             raise RuntimeError(f"SMRAM must be at least 64kb in size")
 
         if ql.mem.is_available(self.smbase, self.smram_size):
-            # Reserve SMRAM and create the SMM heap. The SMM heap will occupy the upper portion of SMRAM.
+            # Reserve SMRAM.
             ql.mem.map(self.smbase, self.smram_size - self.heap_size)
+            # Create the SMM heap, which will occupy the upper portion of SMRAM.
             self.heap = QlMemoryHeap(ql, self.smbase + self.smram_size - self.heap_size, self.smbase + self.smram_size)
         else:
             raise RuntimeError(f"Can't allocate SMRAM at 0x{self.smbase:x}-0x{self.smbase+self.smram_size:x}, \
 region is already occupied")
+
+        # Points to an optional handler context which was specified when the
+        # handler was registered.
+        self.context_buffer = self.heap.alloc(self.PAGE_SIZE)
+
+        # A pointer to a collection of data in memory that will
+        # be conveyed from a non-MM environment into an MM environment.
+        self.comm_buffer = self.heap.alloc(self.PAGE_SIZE)
 
 def init(ql, in_smm=False):
     ql.os.smm = SmmState(ql)
--- smm/protocols/smm_sw_dispatch2_protocol.py
+++ smm/protocols/smm_sw_dispatch2_protocol.py
@@ -6,6 +6,7 @@ from qiling.os.uefi.fncc import *
 
 pointer_size = ctypes.sizeof(ctypes.c_void_p)
 
+
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
     "DispatchFunction": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
@@ -13,9 +14,49 @@ pointer_size = ctypes.sizeof(ctypes.c_void_p)
     "DispatchHandle": POINTER, #POINTER_T(POINTER_T(None))
 })
 def hook_SMM_SW_DISPATCH2_Register(ql, address, params):
+    # Read 'this' pointer.
+    this = EFI_SMM_SW_DISPATCH2_PROTOCOL.from_buffer(
+        ql.mem.read(params["This"], ctypes.sizeof(EFI_SMM_SW_DISPATCH2_PROTOCOL)))
+
+    # Read registration context.
+    smm_sw_register_context = EFI_SMM_SW_REGISTER_CONTEXT.from_buffer(
+        ql.mem.read(params['RegisterContext'], ctypes.sizeof(EFI_SMM_SW_REGISTER_CONTEXT)))
+
+    if smm_sw_register_context.SwSmiInputValue == 0xFFFFFFFF_FFFFFFFF:
+        raise NotImplementedError("SwSmiInputValue == 0xFFFFFFFF_FFFFFFFF")
+    else:
+        registered = [handler['RegisterContext'].SwSmiInputValue for handler in ql.os.smm.swsmi_handlers.values()]
+        if smm_sw_register_context.SwSmiInputValue in registered:
+            # SMI# is already registered.
+            return EFI_INVALID_PARAMETER
+
+        if smm_sw_register_context.SwSmiInputValue > this.MaximumSwiValue:
+            # SMI# is too big.
+            return EFI_INVALID_PARAMETER
+
+    smm_sw_context = EFI_SMM_SW_CONTEXT()
+    # For now we only support 1 CPU.
+    smm_sw_context.SwSmiCpuIndex = 0
+    # CommandPort is the value that was written to the APM I/O port 0xB2.
+    # For software SMIs, it is used to signal which SMI handler is to be invoked.
+    smm_sw_context.CommandPort = smm_sw_register_context.SwSmiInputValue
+    # DataPort is the value that was written to the APM I/O port 0xB3.
+    # This is a scratchpad register used to pass additional data to the SMI handler.
+    # Currently we don't support it and always pass a zero value for it.
+    smm_sw_context.DataPort = 0
+
+    # Allocate a unique handle for this SMI.
+    dispatch_handle = ql.os.heap.alloc(1)
+    write_int64(ql, params["DispatchHandle"], dispatch_handle)
+    
+    smi_params = {
+        "DispatchFunction": params["DispatchFunction"],
+        "RegisterContext": smm_sw_register_context,
+        "CommunicationBuffer": smm_sw_context,
+    }
+
     # Let's save the dispatch params, so they can be triggered if needed.
-    smi_num = int.from_bytes(ql.mem.read(params['RegisterContext'], 8), 'little')
-    ql.os.smm.swsmi_handlers.append((smi_num, params))
+    ql.os.smm.swsmi_handlers[dispatch_handle] = smi_params
     return EFI_SUCCESS
     
 @dxeapi(params={
@@ -25,6 +66,7 @@ def hook_SMM_SW_DISPATCH2_Register(ql, address, params):
 def hook_SMM_SW_DISPATCH2_UnRegister(ql, address, params):
     try:
         del ql.os.smm.swsmi_handlers[params['DispatchHandle']]
+        ql.os.heap.free(params['DispatchHandle'])
     except:
         return EFI_INVALID_PARAMETER
     else:
@@ -44,5 +86,7 @@ def install_EFI_SMM_SW_DISPATCH2_PROTOCOL(ql, start_ptr):
     ptr += pointer_size
 
     efi_smm_sw_dispatch2_protocol.MaximumSwiValue = 0xFF
+    ptr += pointer_size
+
     return (ptr, efi_smm_sw_dispatch2_protocol)
 
