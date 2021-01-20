1c5e3dc988d2aedf694c8d509632c5c935a107f3
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-01-20 18:18:33 +0200

    Update _patch_device_handle to match new Qiling architecture.

--- rom/__init__.py
+++ rom/__init__.py
@@ -1,19 +1,17 @@
 import ctypes
 from qiling.os.uefi.protocols.EfiLoadedImageProtocol import EFI_LOADED_IMAGE_PROTOCOL
-# from qiling.os.uefi.utils import convert_struct_to_bytes
 from .efi_firmware_volume2_protocol import install_EFI_FIRMWARE_VOLUME2_PROTOCOL
 import uefi_firmware
 
 def _patch_device_handle(ql, device_handle):
     # Patch the DeviceHandle member
     for img in ql.loader.images:
-        loaded_image_protocol_ptr = ql.loader.handle_dict[img.base]['5b1b31a1-9562-11d2-8e3f-00a0c969723b']
-        loaded_image_protocol = EFI_LOADED_IMAGE_PROTOCOL.from_buffer(
-            ql.mem.read(loaded_image_protocol_ptr, ctypes.sizeof(EFI_LOADED_IMAGE_PROTOCOL)))
+        loaded_image_protocol_ptr = ql.loader.dxe_context.protocols[img.base]['5b1b31a1-9562-11d2-8e3f-00a0c969723b']
+        loaded_image_protocol = EFI_LOADED_IMAGE_PROTOCOL.loadFrom(ql, loaded_image_protocol_ptr)
         # Patch the DeviceHandle member to refer to the volume driver
         loaded_image_protocol.DeviceHandle = device_handle
         # Write back to memory
-        ql.mem.write(loaded_image_protocol_ptr, convert_struct_to_bytes(loaded_image_protocol))
+        loaded_image_protocol.saveTo(ql, loaded_image_protocol_ptr)
 
 def install(ql, rom_file):
     
