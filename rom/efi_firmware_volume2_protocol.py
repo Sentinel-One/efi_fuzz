02508493a93a09afd8a4d071d2a630d3227fc885
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-01-20 17:45:57 +0200

    Update EFI_FIRMWARE_VOLUME2_PROTOCOL - wip.

--- rom/efi_firmware_volume2_protocol.py
+++ rom/efi_firmware_volume2_protocol.py
@@ -1,11 +1,15 @@
+from smm.protocols.smm_sw_dispatch2_type import EFI_STATUS, EFI_HANDLE
+from qiling.os.uefi.ProcessorBind import FUNCPTR, STRUCT, VOID, PTR, UINT8, UINTN, UINT32
 from qiling.os.uefi.const import EFI_BUFFER_TOO_SMALL, EFI_NOT_FOUND, EFI_SUCCESS, EFI_UNSUPPORTED
-from .efi_firmware_volume2_type import EFI_FIRMWARE_VOLUME2_PROTOCOL
+# from .efi_firmware_volume2_type import EFI_FIRMWARE_VOLUME2_PROTOCOL
 import ctypes
 from qiling.os.uefi.fncc import *
 from qiling.os.uefi.utils import read_int64, write_int64
 from qiling.os.const import *
 import uefi_firmware
 from enum import Enum
+
+EFI_FIRMWARE_VOLUME2_PROTOCOL_GUID = '220e73b6-6bdb-4413-8405-b974b108619a'
 
 class EFI_SECTION_TYPE(Enum):
     PE32                  = 0x10
@@ -21,23 +25,38 @@ class EFI_SECTION_TYPE(Enum):
     PEI_DEPEX             = 0x1B
     SMM_DEPEX             = 0x1C
 
+class EFI_FIRMWARE_VOLUME2_PROTOCOL(STRUCT):
+    EFI_FIRMWARE_VOLUME2_PROTOCOL = STRUCT
+    _fields_ = [
+        ('GetVolumeAttributes', FUNCPTR(EFI_STATUS, PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), PTR(VOID))),
+        ('SetVolumeAttributes', FUNCPTR(EFI_STATUS, PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), PTR(VOID))),
+        ('ReadFile',            FUNCPTR(EFI_STATUS, PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), PTR(VOID), PTR(VOID), PTR(VOID), PTR(VOID), PTR(VOID), PTR(VOID))),
+        ('ReadSection',         FUNCPTR(EFI_STATUS,  PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), PTR(VOID), UINT8, UINTN, PTR(VOID), PTR(VOID), PTR(VOID))),
+        ('WriteFile',           FUNCPTR(EFI_STATUS, PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), UINT32, UINT32, UINTN, PTR(VOID))),
+        ('GetNextFile',         FUNCPTR(EFI_STATUS, PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), PTR(VOID), PTR(VOID), PTR(VOID), PTR(VOID), PTR(VOID))),
+        ('KeySize',             UINT32),
+        ('ParentHandle',        EFI_HANDLE),
+        ('GetInfo',             FUNCPTR(EFI_STATUS, PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), PTR(VOID), PTR(VOID), PTR(VOID))),
+        ('SetInfo',             FUNCPTR(EFI_STATUS, PTR(EFI_FIRMWARE_VOLUME2_PROTOCOL), PTR(VOID), UINTN, PTR(VOID))),
+]
+
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "FvAttributes": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "FvAttributes": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
 })
 def hook_GetVolumeAttributes(ql, address, params):
     return EFI_UNSUPPORTED
 
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "FvAttributes": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "FvAttributes": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
 })
 def hook_SetVolumeAttributes(ql, address, params):
     return EFI_UNSUPPORTED
 
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "NameGuid": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "NameGuid": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
     "Buffer": POINTER,
     "BufferSize": POINTER,
     "FoundType": POINTER,
@@ -64,7 +83,7 @@ def get_section(fw_file, section_type, section_instance):
 
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "NameGuid": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "NameGuid": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
     "SectionType": INT,
     "SectionInstance": INT,
     "Buffer": POINTER,
@@ -106,7 +125,7 @@ def hook_ReadSection(ql, address, params):
 
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "NumberOfFiles": INT, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "NumberOfFiles": INT, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
     "WritePolicy": INT,
     "FileData": POINTER,
 })
@@ -115,7 +134,7 @@ def hook_WriteFile(ql, address, params):
 
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "Key": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "Key": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
     "FileType": POINTER,
     "NameGuid": POINTER,
     "Attributes": POINTER,
@@ -126,7 +145,7 @@ def hook_GetNextFile(ql, address, params):
 
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "InformationType": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "InformationType": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
     "BufferSize": POINTER,
     "Buffer": POINTER,
 })
@@ -135,48 +154,28 @@ def hook_GetInfo(ql, address, params):
 
 @dxeapi(params={
     "This": POINTER, #POINTER_T(struct__EFI_SMM_SW_DISPATCH2_PROTOCOL)
-    "InformationType": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, POINTER_T(None), POINTER_T(None), POINTER_T(None), POINTER_T(ctypes.c_uint64)))
+    "InformationType": POINTER, #POINTER_T(ctypes.CFUNCTYPE(ctypes.c_uint64, PTR(VOID), PTR(VOID), PTR(VOID), POINTER_T(ctypes.c_uint64)))
     "BufferSize": INT,
     "Buffer": POINTER,
 })
 def hook_SetInfo(ql, address, params):
     return EFI_UNSUPPORTED
 
-def install_EFI_FIRMWARE_VOLUME2_PROTOCOL(ql, start_ptr):
-    efi_firmware_volume2_protocol = EFI_FIRMWARE_VOLUME2_PROTOCOL()
-    ptr = start_ptr + ctypes.sizeof(EFI_FIRMWARE_VOLUME2_PROTOCOL)
-    pointer_size = 8
-
-    efi_firmware_volume2_protocol.GetVolumeAttributes = ptr
-    ql.hook_address(hook_GetVolumeAttributes, ptr)
-    ptr += pointer_size
-
-    efi_firmware_volume2_protocol.SetVolumeAttributes = ptr
-    ql.hook_address(hook_SetVolumeAttributes, ptr)
-    ptr += pointer_size
-
-    efi_firmware_volume2_protocol.ReadFile = ptr
-    ql.hook_address(hook_ReadFile, ptr)
-    ptr += pointer_size
-
-    efi_firmware_volume2_protocol.ReadSection = ptr
-    ql.hook_address(hook_ReadSection, ptr)
-    ptr += pointer_size
-
-    efi_firmware_volume2_protocol.WriteFile = ptr
-    ql.hook_address(hook_WriteFile, ptr)
-    ptr += pointer_size
-
-    efi_firmware_volume2_protocol.GetNextFilen = ptr
-    ql.hook_address(hook_GetNextFile, ptr)
-    ptr += pointer_size
-
-    efi_firmware_volume2_protocol.GetInfo = ptr
-    ql.hook_address(hook_GetInfo, ptr)
-    ptr += pointer_size
-
-    efi_firmware_volume2_protocol.SetInfo = ptr
-    ql.hook_address(hook_SetInfo, ptr)
-    ptr += pointer_size
-
-    return (ptr, efi_firmware_volume2_protocol)
+def install_EFI_FIRMWARE_VOLUME2_PROTOCOL(ql):
+    descriptor = {
+        'guid'   : EFI_FIRMWARE_VOLUME2_PROTOCOL_GUID,
+        'struct' : EFI_FIRMWARE_VOLUME2_PROTOCOL,
+        'fields' : (
+            ('GetVolumeAttributes', hook_GetVolumeAttributes),
+            ('SetVolumeAttributes', hook_SetVolumeAttributes),
+            ('ReadFile',            hook_ReadFile),
+            ('ReadSection',         hook_ReadSection),
+            ('WriteFile',           hook_WriteFile),
+            ('GetNextFile',         hook_GetNextFile),
+            # ('KeySize',             
+            # ('ParentHandle',       
+            ('GetInfo',             hook_GetInfo),
+            ('SetInfo',             hook_SetInfo),
+        )
+    }
+    ql.loader.dxe_context.install_protocol(descriptor, 1)
