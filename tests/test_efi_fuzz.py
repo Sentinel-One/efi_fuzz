e17f82d295b995a616e008690c749327eb5dcc50
carlsbad <carlsbad@sentinelone.com>
2021-01-27 00:49:27 +0200

    Add test for UEFI_FIRMWARE_VOLUME2_PROTOCOL.

--- tests/test_efi_fuzz.py
+++ tests/test_efi_fuzz.py
@@ -7,14 +7,18 @@ except ImportError:
 
 from qiling import Qiling
 from qiling.const import QL_INTERCEPT
+from qiling.os.uefi.utils import read_int64, write_int64
 
 import sys
 sys.path.append('..')
 from taint.tracker import *
 import mockito
 import taint
+import rom
 
 def test_uninitialized_memory_tracker():
+    TEST_NAME = b'UninitializedMemoryTracker'
+
     enable_trace = True
     ql = Qiling(['./bin/EfiFuzzTests.efi'],
                 ".",                                        # rootfs
@@ -24,7 +28,7 @@ def test_uninitialized_memory_tracker():
                 output='debug')
 
     # NVRAM environment.
-    ql.env.update({'TestName': b'UninitializedMemoryTracker', 'foo': b'\xde\xad\xbe\xef'})
+    ql.env.update({'TestName': TEST_NAME, 'foo': b'\xde\xad\xbe\xef'})
 
     def validate_taint_set_variable(ql, address, params):
         assert params['VariableName'] == 'bar' and params['DataSize'] == 0x14
@@ -47,3 +51,35 @@ def test_uninitialized_memory_tracker():
 
     # Make sure that SetVariable() was intercepted once.
     mockito.verify(set_variable_spy, times=1).__call__(*mockito.ARGS)
+
+def test_firmware_volume():
+    TEST_NAME = b'FirmwareVolume'
+
+    enable_trace = True
+    ql = Qiling(['./bin/EfiFuzzTests.efi'],
+                ".",                                        # rootfs
+                console=True if enable_trace else False,
+                stdout=1 if enable_trace else None,
+                stderr=1 if enable_trace else None,
+                output='debug')
+
+    # NVRAM environment.
+    ql.env.update({'TestName': TEST_NAME})
+
+    rom.install(ql, "./bin/$0AGD000.FL1")
+
+    def validate_read_section(ql, address, params):
+        buffer_size = read_int64(ql, params['BufferSize'])
+        buffer = ql.mem.read(read_int64(ql, params['Buffer']), buffer_size)
+        assert buffer.decode('utf-16').strip('\x00') == 'DxeMain.efi'
+        return (address, params)
+
+    # Hook ReadSection() to check the taint on the buffer.
+    read_section_spy = mockito.spy(validate_read_section)
+    ql.set_api("ReadSection", read_section_spy, QL_INTERCEPT.EXIT)
+
+    # okay, ready to roll.
+    ql.run()
+
+    # Make sure that ReadSection() was intercepted once.
+    mockito.verify(read_section_spy, times=1).__call__(*mockito.ARGS)
