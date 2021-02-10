5d56b8ce100f62be73d5a01654cca327822b5e20
carlsbad <carlsbad@sentinelone.com>
2021-02-10 12:43:44 +0200

    Pass --taint uninitialized to the NVRAM fuzzing example.

--- examples/nvram/run.sh
+++ examples/nvram/run.sh
@@ -2,4 +2,4 @@
 
 rm -rf afl_outputs
 afl-fuzz -i afl_inputs -o afl_outputs -U -- \
-python ../../efi_fuzz.py fuzz PlatformInitDxe.efi -x S3SaveStateDxe.efi -j PlatformInitDxe.json nvram SetupVolatileData @@
+python ../../efi_fuzz.py fuzz PlatformInitDxe.efi -x S3SaveStateDxe.efi --taint uninitialized -j PlatformInitDxe.json nvram SetupVolatileData @@
