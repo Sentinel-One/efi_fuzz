068f0e20db1a1981f3e77d8d7d8bd7724f118e59
carlsbad <carlsbad@sentinelone.com>
2021-02-10 18:00:34 +0200

    Return just leaf subclasses.

--- sanitizers/__init__.py
+++ sanitizers/__init__.py
@@ -9,8 +9,10 @@ def get_available_sanitizers():
         all_subclasses = []
 
         for subclass in cls.__subclasses__():
-            all_subclasses.append(subclass)
-            all_subclasses.extend(get_all_subclasses(subclass))
+            if not get_all_subclasses(subclass):            
+                all_subclasses.append(subclass)
+            else:
+                all_subclasses.extend(get_all_subclasses(subclass))
 
         return all_subclasses
 
